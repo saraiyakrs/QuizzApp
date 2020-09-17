d60e10da9ba09fb5026e2b77330f19bcbf392def
saraiyakrs <56649573+saraiyakrs@users.noreply.github.com>
2020-09-17 08:30:49 -0400

    Added score counter

--- QuizApp.java
+++ QuizApp.java
@@ -13,51 +13,51 @@ public class QuizApp {
            System.out.println("Hello");
            System.out.println("What color is the sky?");
            String answer1 = kboard.nextLine();
-           if(answer1.equals("blue")) {
+           if(answer1.equals("Blue ")) {
                //give them a point
                score++;
                System.out.println("correct!");
           
            }
-           else {
+            
                System.out.println("What planet are you living on?");
                String answer2 = kboard.nextLine();
-               if(answer2.equals("Earth")) {
+               if(answer2.equals("Earth ")) {
                    //give another point
                    score++;
                    System.out.println("Correct!");
            }
-           else {
+           
                System.out.println("What galaxy do we live in?");
                String answer3 = kboard.nextLine();
-               if(answer3.equals("Milky Way")) {
+               if(answer3.equals("MilkyWay ")) {
                    //give another point
                    score++;
                    System.out.println("Correct!"); 
            
            }
-               else {
+               
                System.out.println("Does the Earth have a moon?");
                String answer4 = kboard.nextLine();
-               if(answer3.equals("Yes")) {
+               if(answer4.equals("Correct ")) {
                    //give another point
                    score++;
                    System.out.println("Correct!"); 
            
             }
-               else {
                System.out.println("How many planets are in the solar system");
                String answer5 = kboard.nextLine();
-               if(answer3.equals("8")) {
+               if(answer5.equals("8 ")) {
                    //give another point
                    score++;
                    System.out.println("Correct!"); 
-                
+               }
                    //Score 
-            }
                
-               
-            }
+              System.out.println("Your score is  "+score+" points"); 
+                
+                    
+           
          
            
     }
